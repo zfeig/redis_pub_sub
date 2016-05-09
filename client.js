3c0bfa6766a4003c8c62e6861f383e7b2e5774a0
zhangfei <zhangfei@calfmobile.com>
2016-05-09 16:45:22 +0800

    update client

--- client.js
+++ client.js
@@ -7,55 +7,85 @@ let _ = require('underscore');
 _.str = require('underscore.string');
 _.v = require('validator');
 let config = require('./config/index');
-let sub= require('./db/redis');
+const sub= require('./db/redis');
 var mysql = require('./db/mysql');
+var wrap = require('co-redis');
 
-function Dispatcher(cb) {
-    co(function* () {
-     
-        //监听频道消息    	
+function* popAndQuery(){
+	 //执行队列操作，更新订单状态
+    		 
+    		  	 let info = yield mysql.query("select * from order_code limit 1");
+    		  	 console.log(JSON.parse(JSON.stringify(info)));
+
+    		  	  let ret = yield sub.RPOP(C.queue).exec();
+                 console.log('ret is %s',ret);
+}
+
+
+     //监听频道消息    	
     	sub.on("message",function(channel,message){
-    		  console.log("收到来自频道：%s的消息：%s",channel,message);
+    	   console.log("收到来自频道：%s的消息：%s",channel,message); 
+          
+             try{
+                    //sub模式下开启新的客户端
+	    			sub.duplicate(function(err,client){
+	    				co(function*(){
+	    					
+	    					//客户端重新包装成promise
+	    					client = wrap(client);
+	    					
+	    					//取队列订单号
+	             			let order_code =  yield client.RPOP(C.queue);
+	             			console.log("从队列弹出订单号：%s",order_code);
+	             			
+	             			//根据订单号查询订单表数据
+	             			if(order_code){
+	             			 let ret = yield mysql.query("update order_code set status = 1,timeline = UNIX_TIMESTAMP() where order_code =?",[order_code]);
+	             			 if(ret.affectedRows > 0){
+	             			 	console.log('update ok!!');
+	             			 }
+
+	             			}
+
+
+
+             		      }).catch(function(err){console.log(err)});
+
+    
+             		});
+
+
+             		
+             		
+     
+
+
+
+
+
+
+
+
+             }catch(err){
+             		console.log(err);
+             }
+          
+           
+
     	});
-       
-    	//订阅频道
+
+
+    
+    //订阅频道
+    co(function* () {
     	let ret = yield sub.subscribe(C.channel);
     	if(ret){
     		console.log("成功订阅了频道：%s",ret);
     	}
-    	
 
-      }).
-    catch(function(err){
+      }).catch(function(err){
     	console.log(err);
     });
-    cb();
-}
+  
 
 
-Dispatcher(function(){
-	//console.log('get data!');
-});
-
-
-
-// //【主任务计时器，启动主任务】
-// let timer_sched = later.parse.recur().every(C.client.time).second();
-// let t = later.setInterval(function () { 
-//    	Dispatcher(function(){
-   		
-//    	});	
-// }, timer_sched);
-
-
-
-
-	// sub.on("subscibe",function(channel,count){
- //    			console.log("订阅了：%s频道，订阅数：%s",channel,count);
- //    	});
-
-	// sub.on("message",function(channel,message){
-	// 	  console.log("收到来自频道：%s的消息：%s",channel,message);
-	// });
-
- //   sub.subscribe(C.channel);
